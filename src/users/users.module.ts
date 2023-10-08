4acd98408722f78c7f9c59d191f31031a187d85b
Mohand <mohand.abdiche@evercam.io>
2023-10-08 14:58:40 +0100

    removing unused provider from users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UsersRepository } from './users.repository';
-import { TransformInterceptor } from './interceptors/transform.interceptor';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository, TransformInterceptor],
+  providers: [UsersService, UsersRepository],
 })
 export class UsersModule {}
